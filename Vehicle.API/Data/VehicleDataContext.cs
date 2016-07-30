13175393bf993aa206f107448a9d71df67be6dbb
Nick Casey <nick_casey@ymail.com>
2016-07-29 21:25:59 -0700

    include unit test folder

---
+++ Vehicle.API/Data/VehicleDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Vehicle.API.Data
+{
+    // 1. Setup class that inherits from DbContext
+    public class VehicleDataContext : DbContext
+    {
+        // 2. Setup constructor 
+        public VehicleDataContext() : base("Vehicle")
+        {
+
+        }
+
+        // 3. Descibe our tables
+        public IDbSet<Models.Vehicle> Vehicles { get; set; }
+
+    }
+}
